e5803130a1a33b824b7c74b00a3a50f721b5831e
Rahul Mainwal <mahatmaji360@gmail.com>
2023-01-14 18:18:32 +0530

    first commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ scheduler.schedule(" 0 0 7 * * * ", function () {
     // Message object
     let message = {
       from: "Computer <computer@service.mail>",
-      to: "mahatmaji360@gmail.com",
+      to: `${process.env.RECEIVER_EMAIL_ID}`,
       subject: "Special for you.",
       text: `Good Morning!`,
       html: `
