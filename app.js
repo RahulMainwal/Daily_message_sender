0d551720522755ef5412580341df369879e74092
RahulMainwal <57298309+RahulMainwal@users.noreply.github.com>
2023-01-14 19:18:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json()).use(cors());
 app.get("/", (req, res) => {
   res.json("Hello, Server is connected ! 👋");
 });
-scheduler.schedule(" * 15 19 * * * ", function () {
+scheduler.schedule(" 0 23 19 * * * ", function () {
   nodemailer.createTestAccount((err, account) => {
     if (err) {
       console.error("Failed to create a testing account. " + err.message);
